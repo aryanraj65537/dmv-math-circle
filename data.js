3b1517c5c9aa2f49eaead54904b16fabb0641d9d
AryKantKode <ary.r.1103@gmail.com>
2024-08-25 01:38:12 -0400

    fixp2

--- data.js
+++ data.js
@@ -4,22 +4,3 @@ const statsData = {
   members: 300,
   workers: 15
 };
-
-const imageFiles = [
-  'adithyaprabha.png',
-  'aidenfeyerherm.png',
-  'alexwang.png',
-  'amoghakella.png',
-  'arnavadepu.png',
-  'arnaviyengar.png',
-  'aryanraj.png',
-  'gigachad.png',
-  'lucasqi.png',
-  'manilsinghprasad.png',
-  'mohanparathasy.png',
-  'nopicyet.png',
-  'roselynchen.png',
-  'sohamkyada.png',
-  'thanhcan.png',
-  'tonyzhang.png'
-];
---
+++ image.js
@@ -0,0 +1,19 @@
+
+const imageFiles = [
+    'adithyaprabha.png',
+    'aidenfeyerherm.png',
+    'alexwang.png',
+    'amoghakella.png',
+    'arnavadepu.png',
+    'arnaviyengar.png',
+    'aryanraj.png',
+    'gigachad.png',
+    'lucasqi.png',
+    'manilsinghprasad.png',
+    'mohanparathasy.png',
+    'nopicyet.png',
+    'roselynchen.png',
+    'sohamkyada.png',
+    'thanhcan.png',
+    'tonyzhang.png'
+  ];  
