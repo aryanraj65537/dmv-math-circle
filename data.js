fc669c5ba1de4a70f0857bb053ca99c57c3e2e2c
AryKantKode <ary.r.1103@gmail.com>
2024-08-25 01:33:39 -0400

    404funny

--- data.js
+++ data.js
@@ -4,3 +4,22 @@ const statsData = {
   members: 300,
   workers: 15
 };
+
+const imageFiles = [
+  'adithyaprabha.png',
+  'aidenfeyerherm.png',
+  'alexwang.png',
+  'amoghakella.png',
+  'arnavadepu.png',
+  'arnaviyengar.png',
+  'aryanraj.png',
+  'gigachad.png',
+  'lucasqi.png',
+  'manilsinghprasad.png',
+  'mohanparathasy.png',
+  'nopicyet.png',
+  'roselynchen.png',
+  'sohamkyada.png',
+  'thanhcan.png',
+  'tonyzhang.png'
+];
