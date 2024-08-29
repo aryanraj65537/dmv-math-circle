b4eb5a702b96492bb7cc325d3b3ebd3ae7a0408f
AryKantKode <ary.r.1103@gmail.com>
2024-08-29 02:34:21 -0400

    comps and workshops done

---
+++ workshops.js
@@ -0,0 +1,14 @@
+const workshops = [
+    {
+        "title": "Olympiad NT: p-adic Valuation, Orders, and Quadratic Residues",
+        "date": "2024-08-15",
+        "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/fNmLeuPPnQs?si=4gfhhWUV-sBLPyXZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+        "pdfLink": "https://drive.google.com/file/d/1q1wwM6q_5ajiVoMkEsaDcxYv4acY2Azl"
+    },
+    {
+      "title": "Projective Geometry and Triangle Cubics",
+      "date": "2024-08-07",
+      "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/iNXx21Wyl5c?si=NHD67vBIpwHmochi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      "pdfLink": "https://drive.google.com/file/d/1lO7ZzPD7PTnQzc1iSexhye5YcIvFoWB2"
+    },
+  ];  
