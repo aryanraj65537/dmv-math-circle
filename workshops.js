6696e8224b56704d28c2a5dec9849c71b5501525
AryKantKode <ary.r.1103@gmail.com>
2024-09-09 20:07:45 -0400

    new workshop

--- workshops.js
+++ workshops.js
@@ -1,14 +1,20 @@
 const workshops = [
-    {
-        "title": "Olympiad NT: p-adic Valuation, Orders, and Quadratic Residues",
-        "date": "2024-08-15",
-        "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/fNmLeuPPnQs?si=4gfhhWUV-sBLPyXZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-        "pdfLink": "https://drive.google.com/file/d/1q1wwM6q_5ajiVoMkEsaDcxYv4acY2Azl"
-    },
-    {
-      "title": "Projective Geometry and Triangle Cubics",
-      "date": "2024-08-07",
-      "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/iNXx21Wyl5c?si=NHD67vBIpwHmochi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-      "pdfLink": "https://drive.google.com/file/d/1lO7ZzPD7PTnQzc1iSexhye5YcIvFoWB2"
-    },
-  ];  
+  {
+    "title": "Kimberling Triangle Centers at their Best",
+    "date": "2024-09-07",
+    "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/QG4UPv3lAJ0?si=yAqpRvhtf67M7VSv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    "pdfLink": "https://drive.google.com/file/d/1cR8A5j6kcj-WmcuHyWiC5G5H18e_aBpU"
+  },
+  {
+    "title": "Olympiad NT: p-adic Valuation, Orders, and Quadratic Residues",
+    "date": "2024-08-15",
+    "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/fNmLeuPPnQs?si=4gfhhWUV-sBLPyXZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    "pdfLink": "https://drive.google.com/file/d/1q1wwM6q_5ajiVoMkEsaDcxYv4acY2Azl"
+  },
+  {
+    "title": "Projective Geometry and Triangle Cubics",
+    "date": "2024-08-07",
+    "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/iNXx21Wyl5c?si=NHD67vBIpwHmochi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    "pdfLink": "https://drive.google.com/file/d/1lO7ZzPD7PTnQzc1iSexhye5YcIvFoWB2"
+  },
+];
