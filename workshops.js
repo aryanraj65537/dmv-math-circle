55952a353eea4e7709a2bc06dc9ffa39336356d1
aryanraj65537 <ary.r.1103@gmail.com>
2025-09-01 17:10:36 -0400

    workshop page buff

--- workshops.js
+++ workshops.js
@@ -1,4 +1,15 @@
-const workshops = [
+window.workshops = [
+  {
+    "title": "Polynomial Irreducibility: Definitions, Criteria, and Applications",
+    "date": "2025-08-31",
+    "youtubeLink": `<iframe width="560" height="315" src="https://www.youtube.com/embed/LjoeFrgLuKA?si=xm_PIryUWepgOBH6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    "pdfLink": "https://drive.google.com/file/d/1ts6oPkxEXRG4RB6UOgF3fA70wXJcoSX5"
+  },
+  {
+    "title": "Quadri-Figures and QL-Cu1",
+    "date": "2024-12-28",
+    "pdfLink": "https://drive.google.com/file/d/1XIDKGk1KcETwoUtzl0e_K2Okk8tTyTWC"
+  },
   {
     "title": "Kimberling Triangle Centers at their Best",
     "date": "2024-09-07",
