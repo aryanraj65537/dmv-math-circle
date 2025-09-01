2b405c296f6a24b553e41a35e7733bf2d9900876
aryanraj65537 <ary.r.1103@gmail.com>
2025-09-01 17:58:12 -0400

    about page buff

---
+++ team.js
@@ -0,0 +1,151 @@
+window.team = {
+  "members": [
+    {
+      "name": "Amogh Akella",
+      "group": "staff",
+      "img": "assets/img/team/amoghakella.png",
+      "bio": "Amogh Akella is a rising junior at Westwood High School in Austin, Texas. He is a two-time USAJMO qualifier and honorable mention, and a 4-time AIME qualifier. He is also the co-head of problem writing for InteGIRLS. In his free time, he enjoys doing geometry, playing sports such as soccer and cricket, playing chess, coding, and playing the piano. You can find him on discord and AoPS at hgomamogh.",
+      "socials": {
+        "discord": "https://discord.com/users/1146621665921470494",
+        "aops": "https://aops.com/community/user/hgomamogh"
+      }
+    },
+    {
+      "name": "Alex Wang",
+      "group": "staff",
+      "img": "assets/img/team/alexwang.png",
+      "bio": "Alex will put something here later.",
+      "socials": {
+        "discord": "https://discord.com/users/437287284425883666"
+      }
+    },
+    {
+      "name": "Lucas Qi",
+      "group": "staff",
+      "img": "assets/img/team/lucasqi.png",
+      "bio": "Lucas, a cofounder of DMV Math Circle, is an avid enjoyer of mathematics. A three-time AIME qualifier, he loves to learn and help others learn the intricacies of math. He has also participated in AMSP and Ross Mathematics Summer Program. Outside of math, Lucas plays tennis and watches movies.",
+      "socials": {
+        "discord": "https://discord.com/users/598608699031814162",
+        "instagram": "https://instagram.com/lucasqi614"
+      }
+    },
+    {
+      "name": "Arnav Iyengar",
+      "group": "staff",
+      "img": "assets/img/team/arnaviyengar.png",
+      "bio": "Arnav Iyengar is a rising sophomore at Jackson-Reed High School in Washington, DC. He is a one time USAJMO and one time USAMO honorable mention. His favorite topics of math are number theory and functional equations. Aside from math, he enjoys playing the piano, playing chess, and running cross country. You can find him on discord at ai7776 and AoPS at megarnie.",
+      "socials": {
+        "discord": "https://discord.com/users/857629254569426954",
+        "aops": "https://aops.com/community/user/megarnie"
+      }
+    },
+    {
+      "name": "Thanh Can",
+      "group": "staff",
+      "img": "assets/img/team/thanhcan.png",
+      "bio": "Thanh will put something here later.",
+      "socials": {
+        "discord": "https://discord.com/users/693667799201218590"
+      }
+    },
+    {
+      "name": "Aiden Feyerherm",
+      "group": "staff",
+      "img": "assets/img/team/aidenfeyerherm.png",
+      "bio": "Aiden Feyerherm is a senior at McLean High School, where he leads their math club to competitions. He is a 2-time USAJMO Honorable Mention, and his favorite topic is combinatorics. In addition to math, he enjoys learning physics, with a USAPhO Bronze medal, as well as competitive programming. He loves giving back to the community, and has taught math and science to students of many age groups for various organizations. In his free time, he juggles, listens to \"Baby Shark\", and plays Geoguessr and Tetris.",
+      "socials": {
+        "discord": "https://discord.com/users/604145543064715301"
+      }
+    },
+    {
+      "name": "Tony Zhang",
+      "group": "staff",
+      "img": "assets/img/team/tonyzhang.png",
+      "bio": "Tony will put something here later.",
+      "socials": {
+        "discord": "https://discord.com/users/689584310214459411",
+        "aops": "https://aops.com/community/user/plang2008"
+      }
+    },
+    {
+      "name": "Luv Udeshi",
+      "group": "staff",
+      "img": "assets/img/team/luvudeshi.png",
+      "bio": "Luv is a junior at TJ in Virginia. He enjoys competition math and is a four-time AIME qualifier. In 2024, he attended the Ross Mathematics Program. In his free time, Luv enjoys playing basketball and coding.",
+      "socials": {
+        "discord": "https://discord.com/users/842862377926131772",
+        "aops": "https://aops.com/community/user/Taco12"
+      }
+    },
+    {
+      "name": "Kalan Warusa",
+      "group": "staff",
+      "img": "assets/img/team/kalanwarusa.png",
+      "bio": "Kalan Warusa is a sophomore at McLean High School. He likes participating in math competitions such as AMC, HMMT, ARML, PUMaC, and CMIMC. He is a three-time AIME qualifier and has won many awards such as 1st at GMU Calculus Olympiad. In his free time, he enjoys doing robotics and playing musical instruments.",
+      "socials": {}
+    },
+    {
+      "name": "Kevin Chen",
+      "group": "staff",
+      "img": "assets/img/team/kevinchen.png",
+      "bio": "Kevin Chen is a sophomore at Ridge High School in New Jersey. He is a USAJMO Honorable Mention, and his main areas of interest in math are polynomial irreducibility and complex bashing. In his free time, he enjoys playing tennis, composing music, and playing the piano.",
+      "socials": {
+        "discord": "https://discord.com/users/711950483417202731",
+        "instagram": "https://www.instagram.com/kchromatic3/"
+      }
+    },
+
+    /* PR team */
+    {
+      "name": "Mohan Parathasy",
+      "group": "pr",
+      "img": "assets/img/team/mohanparathasy.png",
+      "bio": "Hi everyone! My name’s Mohan, and I’m a current sophomore at TJ. I’ve been really into math since 3rd grade, when I started getting competitive about it, but I’ve never really found my fit within comp math clubs. DMV Math Circle lets me do that, and as part of the PR team, I get to help others do the same. If you have any questions or want to reach out and talk, please send me a note. Thanks!",
+      "socials": {
+        "discord": "https://discord.com/users/800763975255588874"
+      }
+    },
+    {
+      "name": "Arnav Adepu",
+      "group": "pr",
+      "img": "assets/img/team/arnavadepu.png",
+      "bio": "Arnav Adepu is a rising junior at Bergen County Academies in Hackensack, New Jersey. He is a medalist at the World Math Team Championship and a numerous time AIME qualifier. Aside from math, he enjoys traveling, going to tournaments for various fields, and research. You can find him on discord and aops as tienxion.",
+      "socials": {
+        "discord": "https://discord.com/users/890706505463132190",
+        "aops": "https://aops.com/community/user/tienxion"
+      }
+    },
+    {
+      "name": "Roselyn Chen",
+      "group": "pr",
+      "img": "assets/img/team/roselynchen.png",
+      "bio": "Roselyn is a freshman who participates in competition math. She enjoys reading, writing, drawing, and painting.",
+      "socials": {}
+    },
+    {
+      "name": "Manil Singh-Prasad",
+      "group": "pr",
+      "img": "assets/img/team/manilsinghprasad.png",
+      "bio": "Manil will put something here later.",
+      "socials": {
+        "discord": "https://discord.com/users/1162171019029643305"
+      }
+    },
+    {
+      "name": "Gabe Zhang",
+      "group": "pr",
+      "img": "assets/img/team/gabezhang.png",
+      "bio": "Gabe is a junior at Basis Mclean who moved to the DMV area from China 2 years ago. He is a USAJMO qualifier and loves to travel in his free time, having been to all 7 continents of the world. He is also an aviation enthusiast.",
+      "socials": {}
+    },
+
+    /* Apply (always last) */
+    {
+      "type": "apply",
+      "name": "You?",
+      "group": "apply",
+      "img": "assets/img/team/gigachad.png",
+      "bio": "Use this <a href=\"https://forms.gle/UMrdwgwJwdep8tta6\">google form</a> to apply for a DMV Math Circle position."
+    }
+  ]
+}
